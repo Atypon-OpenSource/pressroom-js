83974b664578e50ad922de3b2152ee90e098a1ec
Alf Eaton <aeaton@atypon.com>
2020-06-08 21:05:52 +0000

    Add license with @manuscripts/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,19 @@
+/*!
+ * © 2020 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      modules: true,
-    },
-  },
-  plugins: [
-    'prettier',
-    '@typescript-eslint',
-    'jest',
-    'import',
-    'simple-import-sort',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-  ],
-  rules: {
-    'prettier/prettier': 'warn',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    'sort-imports': 'off',
-    'import/order': 'off',
-    'simple-import-sort/sort': 'error',
-  },
+  extends: '@manuscripts/eslint-config',
 }
